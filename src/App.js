6dfba719c46f16b0e1624d42731faf8940cc0443
llyt <anton@lytkin.me>
2019-12-17 11:22:17 +0300

    Complete Components task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 import React from 'react'
-import data from './products'
-import './index.css'
+import ProductPage from './containers/ProductPage/ProductPage'
 
-const App = () => (
-		<div className='shop'>
-			<h1>Список товаров</h1>
-			<ul>
-				{ data.slice(0, 3).map(product => <li>{product.name}</li>) }
-			</ul>
-		</div>
-  )
-export default App
+export default () => <ProductPage />
---
+++ src/components/ProductList/ProductList.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './ProductList.module.css'
+import ProductItem from '../../containers/ProductItem/ProductItem'
+import data from '../../products'
+import pic1 from '../../img/1.jpg'
+import pic2 from '../../img/2.jpg'
+import pic3 from '../../img/3.jpg'
+import pic4 from '../../img/4.jpg'
+import pic5 from '../../img/5.jpg'
+import pic6 from '../../img/6.jpg'
+
+const pictures = [pic1, pic2, pic3, pic4, pic5, pic6]
+
+const ratingComponent = ({ isFilled }) => isFilled ? <div>&#9733;</div> : <div>&#9734;</div>
+
+const goodsList = data.map((item, index) => {
+	return (
+		<ProductItem 
+			isInStock={item.isInStock}
+			img={pictures[index]}
+			title={item.title}
+			price={item.price}
+			subPriceContent={item.subPriceContent}
+			maxRating={item.maxRating}
+			rating={item.rating}
+			ratingComponent={ratingComponent}
+		/>
+	)
+})
+
+const ProductList = props => (
+		<ul className={styles.ProductList}>
+			{goodsList.map(liItem => 
+				(<li>{liItem}</li>)
+			)}
+		</ul>
+	)
+	
+export default ProductList
