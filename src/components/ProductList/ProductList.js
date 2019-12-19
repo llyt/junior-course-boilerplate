c64302e0074962b425c98d94c46470a411d5a824
llyt <anton@lytkin.me>
2019-12-19 12:00:08 +0300

    Price Filter init 2

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './ProductList.module.css'
 import ProductItem from '../../containers/ProductItem/ProductItem'
 import H1Item from '../../components/UI/H1Item/H1Item'
-import data from '../../products'
 import pic1 from '../../img/1.jpg'
 import pic2 from '../../img/2.jpg'
 import pic3 from '../../img/3.jpg'
@@ -14,28 +13,34 @@ const pictures = [pic1, pic2, pic3, pic4, pic5, pic6]
 
 const ratingComponent = ({ isFilled }) => isFilled ? <div>&#9733;</div> : <div>&#9734;</div>
 
-const goodsList = data.map((item, index) => {
+const ProductList = props => {
+	const goodsList = props.products.map((item, index) => {
+		return (
+			<li key={index}>
+				<ProductItem 
+					isInStock={item.isInStock}
+					img={pictures[index]}
+					title={item.title}
+					price={item.price}
+					subPriceContent={item.subPriceContent}
+					maxRating={item.maxRating}
+					rating={item.rating}
+					ratingComponent={ratingComponent}
+				/>
+			</li>
+		)
+	})
 	return (
-		<ProductItem 
-			isInStock={item.isInStock}
-			img={pictures[index]}
-			title={item.title}
-			price={item.price}
-			subPriceContent={item.subPriceContent}
-			maxRating={item.maxRating}
-			rating={item.rating}
-			ratingComponent={ratingComponent}
-		/>
-	)
-})
-
-const ProductList = props => (
 		<div className={styles.ProductList}>
 			<H1Item content='Список товаров'/>
-			<ul>
-				{ goodsList.map((liItem, index) => <li key={index}>{liItem}</li>) }
-			</ul>
+			{ goodsList === undefined || goodsList.length === 0
+				? <div style={{margin: "40px 0 0 20px"}}>Товары не найдены</div>
+				: <ul>
+						{ goodsList }
+					</ul>
+			 }
 		</div>
 	)
+}
 	
 export default ProductList
