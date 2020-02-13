77631f306c310228f93e70a15c0340a158dda873
llyt <anton@lytkin.me>
2020-02-13 13:11:07 +0300

    Rebase modularization

--- src/containers/SidebarContainer.js
+++ src/containers/SidebarContainer.js
@@ -19,16 +19,6 @@ const mapStateToProps = (state) => (
 )
 
 const mapDispatchToProps = (dispatch) => {
-<<<<<<< HEAD
-	return {
-		inputChange: (name, value) => dispatch({type: "INPUT_CHANGE", payload: {name, value}}),
-		handleCategoryFilter: (event) => {
-			const categoryName = event.target.innerHTML
-			dispatch({type: "CATEGORY_FILTER", payload: {categoryName}})
-		},
-		handleReset: () => dispatch({type: "RESET_FILTERS"})
-	}
-=======
   return {
     inputChange: (name, value) => dispatch({ type: 'INPUT_CHANGE', payload: { name, value } }),
     handleCategoryFilter: (event) => {
@@ -37,6 +27,5 @@ const mapDispatchToProps = (dispatch) => {
     },
     handleReset: () => dispatch({ type: 'RESET_FILTERS' })
   }
->>>>>>> Added pagination
 }
 export const SidebarContainer = connect(mapStateToProps, mapDispatchToProps)(Sidebar)
