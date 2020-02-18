376a8098f33a95f842c8a55f843b56afe449b519
llyt <anton@lytkin.me>
2020-02-18 18:47:26 +0300

    Cleaning up after refactor

--- src/store/modules/productList.js
+++ src/store/modules/productList.js
@@ -1,14 +1,13 @@
 import dataJSON from '../../products'
-import queryString from 'query-string'
 import { splitEvery } from 'csssr-school-utils'
 
 const getProductsList = (state) => {
-  const { products, filters } = state
-  const { data } = products
-  const { params, minPrice, maxPrice, discount } = filters
-  const activeCategories = params.category
+  const { products, filters } = state;
+  const { data } = products;
+  const { params, minPrice, maxPrice, discount } = filters;
+  const activeCategories = params.category;
 
-  let resultProducts = data
+  let resultProducts = data;
 
   if (activeCategories && activeCategories.length !== 0) {
     resultProducts = resultProducts.filter(({category}) => activeCategories.includes(category))
@@ -18,12 +17,12 @@ const getProductsList = (state) => {
     return price >= minPrice && price <= maxPrice * (1 - discount / 100)
   })
 
-}
+};
 
 const initialState = {
   data: dataJSON,
   perPage: 6
-}
+};
 
 // Reducers
 
@@ -37,39 +36,9 @@ export default (state = initialState, action) => {
 // Selectors
 
 export const makeProductList = (state) => {
-  const { perPage } = state.products
+  const { perPage } = state.products;
 
-  const filteredProducts = getProductsList(state)
+  const filteredProducts = getProductsList(state);
   return splitEvery(perPage, filteredProducts)
 
-}
-
-export const makePagination = (state, paginationLength) => {
-  const { params } = state.filters
-  const { page } = params
-  const pagination = []
-
-  // Filling an array with array of links
-  for (let i = 1; i <= paginationLength; i += 1) {
-    const nextParams = {...params, page: i}
-    const url = queryString.stringify(nextParams, {arrayFormat: 'comma'})
-    const cls = i === page ? 'active' : ''
-    pagination.push([`${i}`, `?${url}`, cls])
-  }
-
-  // Insert 'Назад' link
-  if (page !== 1) {
-    const backParams = {...params, page: page - 1}
-    const backUrl = queryString.stringify(backParams, {arrayFormat: 'comma'})
-    pagination.unshift(['Назад', backUrl, 'prevPage'])
-  }
-
-  // Insert 'Вперед' link
-  if (page !== 1 && page !== paginationLength) {
-    const nextParams = {...params, page: page + 1}
-    const nextUrl = queryString.stringify(nextParams, {arrayFormat: 'comma'})
-    pagination.push(['Вперед', nextUrl, 'nextPage'])
-  }
-
-  return pagination
-}
+};
