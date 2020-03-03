801be527ee5e391607b2b06b2620530bbe01479f
llyt <anton@lytkin.me>
2020-03-03 14:26:04 +0300

    Move getListOfCategories to utils

--- src/store/modules/catalog/reducers.js
+++ src/store/modules/catalog/reducers.js
@@ -1,22 +1,18 @@
 import { initialState } from './index'
+import getListOfCategories from '../../../utils/getListOfCategories'
 import { maxBy } from 'csssr-school-utils'
 
 const mainReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_PRODUCTS':
       const products = action.payload.data
-      const getListOfCategories = (data) => {
-        const allCategoriesSet = new Set(data.map(({ category }) => category));
-        const unSortedList = [...allCategoriesSet];
-
-        return unSortedList.sort((a, b) => (a.name > b.name ? 1 : -1) || 0)
-      }
 
       if (products.length === 0) {
         return {
           ...state,
         }
       }
+
       return {
         ...state,
         products: {
